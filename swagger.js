66ba10fe2418a1545b1841df0aa7c17b909edb3f
noaicancode <balinteegor@icloud.com>
2025-02-21 12:52:04 +0500

    new commet

--- swagger.js
+++ swagger.js
@@ -1,22 +1,23 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Email Sending API',
-      version: '1.0.0',
-      description: 'API for sending emails with attachments',
+module.exports = function() {
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Email Sending API',
+        version: '1.0.0',
+        description: 'API for sending emails with attachments',
+      },
+      servers: [
+        {
+          url: process.env.NODE_ENV === 'production' ? 'https://smtp-api.vercel.app' : 'http://localhost:3000',
+          description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server'
+        }
+      ],
     },
-    servers: [
-      {
-        url: process.env.NODE_ENV === 'production' ? 'https://smtp-api.vercel.app' : 'http://localhost:3000',
-        description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server'
-      }
-    ],
-  },
-  apis: ['./index.js'],
-};
+    apis: ['./index.js'],
+  };
 
-const specs = swaggerJsdoc(options);
-module.exports = specs;
+  return swaggerJsdoc(options);
+};
